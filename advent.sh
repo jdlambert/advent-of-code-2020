083504c9a9412d293bd6e82e9cd439f460446a35
Jacob Lambert <jacob.lambert@gmail.com>
2020-12-01 10:59:29 -0500

    Add a template and a helper script

---
+++ advent.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+[[ $# -eq 0 ]] && echo "Usage: ./advent.sh day"
+
+echo "DAY $1"
+DIR=$(printf "day%02d" $1)
+
+RED="\e[31m"
+GREEN="\e[32m"
+
+if [ ! -d $DIR ]; then
+  URL=https://adventofcode.com/2019/day/$1
+  cp -r template $DIR
+  curl -f $URL/input -H "cookie: $(cat cookie)" > $DIR/input.txt 2> /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Input not available!"
+    rm -rf $DIR
+    exit 1
+  else
+    echo "Directory created!"
+    exit 0
+  fi
+fi
+
+cd $DIR
+[ -f solution.cpp ] && printf "$RED\nC++\n" && make solution && ./solution
+[ -f solution.goj ] && printf "$GREEN\nGo\n" && gofmt -w solution.go && go run solution.go
---
+++ template/solution.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main {
+	reader, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(reader)
+}
