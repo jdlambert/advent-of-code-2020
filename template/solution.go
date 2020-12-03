0dd807d7c10601f692711fdd2078c2c12d92e33e
Jacob Lambert <jacob.lambert@gmail.com>
2020-12-03 17:10:30 -0500

    Update go template

--- template/solution.go
+++ template/solution.go
@@ -9,5 +9,7 @@ import (
 func main() {
 	reader, _ := os.Open("input.txt")
 	scanner := bufio.NewScanner(reader)
-	fmt.Println("Hello, world!")
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
